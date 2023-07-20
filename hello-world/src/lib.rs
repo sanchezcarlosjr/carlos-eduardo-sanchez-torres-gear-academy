dbb49055e94038d4f137fb76518df9cc7f1a3382
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-07-20 03:58:50 +0000

    feat(lesson): Testing the updated smart contract and More advanced Hello world

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,16 +1,45 @@
 #![no_std]
-use gstd::{msg, prelude::*, debug};
+use gstd::{msg, prelude::*, debug,ActorId};
 
-#[no_mangle]
-extern "C" fn handle() {
-    msg::reply(String::from("Hello"), 0)
-        .expect("Error in sending a reply message");
+#[derive(Encode, Decode, TypeInfo, Debug)]
+pub enum InputMessages {
+    // ActorID is 256-bit identifier https://docs.gear.rs/gstd/struct.ActorId.html
+    SendHelloTo(ActorId),
+    TEST,
+    SendHelloReply,
 }
+
+static mut GREETING: Option<String> = None;
 
 #[no_mangle]
 extern "C" fn init() {
-    let init_message: String = msg::load()
-        .expect("Can't load init message");
-    debug!("Program was initialized with message {:?}",
-        init_message);
+    let greeting: String = msg::load().expect("Can't decode an init message");
+    debug!("Program was initialized with message {:?}",greeting);
+    unsafe { GREETING = Some(greeting) };
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+    let greeting = unsafe {
+        GREETING
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+    debug!("HANDLE 1 {:?}", greeting);
+    let input_message: InputMessages = msg::load().expect("Error in loading InputMessages");
+    debug!("HANDLE 2 {:?}", input_message);
+    match input_message {
+        InputMessages::SendHelloTo(account) => {
+            debug!("Message: SendHelloTo {:?}", account);
+            msg::send(account, greeting, 0).expect("Error in sending Hello message to account");
+        }
+        InputMessages::SendHelloReply => {
+            debug!("Message: SendHelloReply");
+            msg::reply(greeting, 0).expect("Error in sending reply");
+        }
+        InputMessages::TEST => {
+            debug!("Message: TEST");
+        }
+    }
+   
 }
