2d5c7783abd852040db8ed6400f8c1e58b09b22f
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-07-25 22:23:30 -0700

    feat(hello world): Understanding the program metadata and state

---
+++ 1-hello-world/io/src/lib.rs
@@ -0,0 +1,23 @@
+#![no_std]
+
+use codec::{Decode, Encode};
+use gmeta::{In, InOut, Metadata};
+use gstd::{prelude::*, ActorId};
+use scale_info::TypeInfo;
+pub struct ProgramMetadata;
+
+impl Metadata for ProgramMetadata {
+    type Init = In<String>;
+    type Handle = InOut<InputMessages, String>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+    type State = String;
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum InputMessages {
+    SendHelloTo(ActorId),
+    SendHelloReply,
+    TEST
+}
