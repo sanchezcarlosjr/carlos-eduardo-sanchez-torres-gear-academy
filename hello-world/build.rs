bf6475d5c00814a569bde02d2016ea44b03ad7c1
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-07-26 02:44:57 +0000

    feat: new state

--- hello-world/build.rs
+++ hello-world/build.rs
@@ -1,3 +1,5 @@
+use hello_world_io::ProgramMetadata;
+
 fn main() {
-    gear_wasm_builder::build();
+    gear_wasm_builder::build_with_metadata::<ProgramMetadata>();
 }
--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,13 +1,6 @@
 #![no_std]
 use gstd::{msg, prelude::*, debug,ActorId};
-
-#[derive(Encode, Decode, TypeInfo, Debug)]
-pub enum InputMessages {
-    // ActorID is 256-bit identifier https://docs.gear.rs/gstd/struct.ActorId.html
-    SendHelloTo(ActorId),
-    TEST,
-    SendHelloReply,
-}
+use hello_world_io::InputMessages;
 
 static mut GREETING: Option<String> = None;
 
@@ -16,6 +9,16 @@ extern "C" fn init() {
     let greeting: String = msg::load().expect("Can't decode an init message");
     debug!("Program was initialized with message {:?}",greeting);
     unsafe { GREETING = Some(greeting) };
+}
+
+#[no_mangle]
+extern "C" fn state() {
+    let greeting = unsafe {
+        GREETING
+            .as_ref()
+            .expect("The contract is not initialized")
+    };
+    msg::reply(greeting, 0).expect("Failed to share state");
 }
 
 #[no_mangle]
